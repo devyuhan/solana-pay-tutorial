50c63bfb4a568c420d8ba965c460f7aa10aae0d5
Yuhan Wang <yuhanwang@Yuhans-MacBook-M1.local>
2022-05-01 18:17:04 -0400

    for deploy

---
+++ lib/addresses.ts
@@ -0,0 +1,6 @@
+import { PublicKey } from "@solana/web3.js"
+
+// Your shop wallet address
+export const shopAddress = new PublicKey('BNABP5hSzSPpXbye531WGFuB3Fzk8FfGfxn8RfGVTpzQ')
+export const usdcAddress = new PublicKey('Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr')
+export const couponAddress = new PublicKey('DAt58JZ8PJZuyc9AY3T5dcdKXhchd2AwBEZLAptBeYmv')
